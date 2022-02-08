d5463087bace160a504e77672042c06b2cef82df
Ankita kadu <ankitakadu131@gmail.com>
2022-02-07 18:28:47 -0800

    add files
    add files

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
       try {
           let res = await fetch(url);
           let data = await res.json();
-          //console.log("data:", data)
+          console.log("data:", data)
           appendMeal(data)
 
       } catch (err) {
